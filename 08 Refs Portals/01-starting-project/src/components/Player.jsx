a607b0fe4fb0c7a74c373078b9cb39761b824476
AhmedAyman95 <aahemdaayman95@gmail.com>
2024-08-06 13:29:48 +0300

    Before Using Refs

--- 08 Refs Portals/01-starting-project/src/components/Player.jsx
+++ 08 Refs Portals/01-starting-project/src/components/Player.jsx
@@ -1,10 +1,25 @@
+import { useState } from "react";
+
 export default function Player() {
+  const [myName, setMyName] = useState("Player1");
+  const [submitted, setSubmitted] = useState(false);
+
+  function handleChange(event) {
+    setSubmitted(false);
+    console.log("Handle Change Is Called");
+    setMyName(event.target.value);
+  }
+
+  function handleClick() {
+    setSubmitted(true);
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {submitted ? myName : "Player1"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" onChange={handleChange} value={myName} />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
